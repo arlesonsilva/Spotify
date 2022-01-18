a354e4693366c851419ea2af1e260f1354275196
Arleson Silva <arlesonsilva@hotmail.com>
2022-01-17 21:01:45 -0300

    initial structure

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Arleson Silva on 17/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
